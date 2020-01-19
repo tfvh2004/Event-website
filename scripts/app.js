7eda8c7acb3e1174618ef404d67743fffd006f54
tfvh2004 <tfvh2004@gmail.com>
2020-01-19 10:12:38 +0100

    added text animation on homepage

--- scripts/app.js
+++ scripts/app.js
@@ -48,3 +48,31 @@ let x = setInterval(function(){
 	let days = Math.floor(distance / (1000 * 60 * 60 * 24));
 	document.getElementById('countdown').innerHTML = days + " Days"
 })
+
+//Making the text animation
+const text = document.querySelector('.tanimation');
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+
+for(let i = 0; i < splitText.length; i++){
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
+};
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+    if(char === splitText.length){
+        complete();
+        return;
+    }
+}
+
+function complete(){
+    clearInterval(timer);
+    timer = null;
+}
